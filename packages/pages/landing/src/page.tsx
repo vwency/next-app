7eff73f2b7650a11de73ee1bf620893a07919c8d
vwency <duckdodo666@gmail.com>
2025-11-29 17:55:18 +0600

    refactor: paddings

--- packages/pages/landing/src/page.tsx
+++ packages/pages/landing/src/page.tsx
@@ -1,10 +1,11 @@
-import React, { useMemo } from 'react'
+import { useMemo } from 'react'
 import { MainTextHead } from '@ui/text-header'
 import { MainText } from '@ui/text-main'
 import { MainSlider } from '@ui/slider'
 import { MainFooterText } from '@ui/text-footer'
 import { MainButtons } from '@ui/buttons'
 import { Footer } from '@ui/footer'
+import styles from './styles/index.module.scss'
 
 export const LandingPage = () => {
   const content = useMemo(
@@ -21,5 +22,9 @@ export const LandingPage = () => {
     []
   )
 
-  return <div className="main__layout no-select">{content}</div>
+  return (
+    <div className={`${styles.container} main__layout no-select`}>
+      {content}
+    </div>
+  )
 }
--- packages/ui/modals/src/DetailedCardModalContent/Items.tsx
+++ packages/ui/modals/src/DetailedCardModalContent/Items.tsx
@@ -2,8 +2,7 @@ import { GalleryItem } from '@shared/interfaces'
 
 export const galleryItems: GalleryItem[] = [
   {
-    image:
-      'https://images.unsplash.com/photo-1747697006653-569b3f418cf9?q=80&w=627&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    image: 'https://st.cherinfo.ru/afisha/prw/10148.jpg',
     alt: 'Природа',
     description: 'Красивый пейзаж с горами и лесом',
     detailedDescription:
