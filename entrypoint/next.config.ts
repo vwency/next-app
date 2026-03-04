677efa4bc47a2ea709716a722d8d2eb92a235d9c
vwency <veniaminshp@gmail.com>
2026-03-04 20:00:03 +0600

    chore: compile optimize

--- entrypoint/next.config.ts
+++ entrypoint/next.config.ts
@@ -7,6 +7,25 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  transpilePackages: [
+    '@ui/footer',
+    '@ui/menu',
+    '@ui/slider',
+    '@ui/hooks',
+    '@ui/buttons',
+    '@ui/advertisements',
+    '@ui/event-board',
+    '@ui/modals',
+    '@ui/stars-background',
+    '@ui/text-footer',
+    '@ui/text-header',
+    '@ui/text-main',
+    '@pages/landing',
+    '@pages/afisha',
+    '@shared/consts',
+    '@shared/interfaces',
+    '@shared/types',
+  ],
 }
 
 export default nextConfig
